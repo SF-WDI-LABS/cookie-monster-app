e7feb2c02bfd8fe233abcec00a482430b9ffcd89
Esther Weon <esther.s.weon@gmail.com>
2017-09-19 12:12:53 -0700

    Solution

--- app/controllers/desserts_controller.rb
+++ app/controllers/desserts_controller.rb
@@ -1,4 +1,6 @@
 class DessertsController < ApplicationController
+  before_action :require_login, except: [:index]
+  # before_action :require_login, only: [:show, :favorite, :save_as_favorite]
 
   # GET /desserts
   def index
@@ -7,7 +9,11 @@ class DessertsController < ApplicationController
 
   # GET /desserts/1
   def show
-    redirect_to root_path and return if not is_logged_in
+    if session[:favorite_dessert_id] == params[:id]
+      @is_favorite = true
+    else
+      @is_favorite = false
+    end
 
     ## CHALLENGE 4: This is my favorite dessert!
     ### Detect whether the dessert the user is viewing
@@ -15,32 +21,35 @@ class DessertsController < ApplicationController
     ### 'FAVORITE dessert' in the `views/desserts/show.html.erb` template
 
     @dessert = Dessert.find(params[:id])
+    # render 'show'
   end
 
   # GET /desserts/favorite
   # Shows the user's favorite dessert (if set in the session)
   def favorite
-    redirect_to root_path and return if not is_logged_in
-
     ## CHALLENGE 3: Render my favorite_dessert!
     ### Check my session for my `favorite_dessert_id`
     ### and then render my favorite dessert
     ### AS-IS this will raise an error
-    favorite_dessert_id = 34404 # CHANGE THIS
+    favorite_dessert_id = session[:favorite_dessert_id] # CHANGE THIS
     @dessert = Dessert.find(favorite_dessert_id)
+    @is_favorite = true
     render 'show'
   end
 
   # POST /desserts/1/save_as_favorite
   # Sets the user's favorite dessert in their session
   def save_as_favorite
-    redirect_to root_path and return if not is_logged_in
     session[:favorite_dessert_id] = params[:id] # SET favorite
     flash[:notice] = "dessert #{params[:id]} saved in session as favorite!"
     redirect_to root_path
   end
 
   private
+
+  def require_login
+    redirect_to root_path and return if not is_logged_in
+  end
 
   def is_logged_in
     cookies[:can_access] == "yeah"
